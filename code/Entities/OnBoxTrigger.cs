fadf2e3c4d7dfa908f9ecd64345d21fe99d43ebd
James Wilkinson <jw@jameswilko.com>
2022-08-25 01:00:59 +0200

    Add on box trigger entity

---
+++ code/Entities/OnBoxTrigger.cs
@@ -0,0 +1,32 @@
+﻿using Sandbox;
+using SandboxEditor;
+
+namespace Garryware.Entities;
+
+[Library("gw_trigger_on_box"), HammerEntity]
+[Title("On Box Trigger")]
+public class OnBoxTrigger : BaseTrigger
+{
+    
+    public bool Contains(Vector3 position)
+    {
+        bool withinX = WorldSpaceBounds.Mins.x <= position.x && WorldSpaceBounds.Maxs.x >= position.x;
+        bool withinY = WorldSpaceBounds.Mins.y <= position.y && WorldSpaceBounds.Maxs.y >= position.y;
+        bool withinZ = WorldSpaceBounds.Mins.z <= position.z && WorldSpaceBounds.Maxs.z >= position.z;
+        return withinX && withinY && withinZ;
+    }
+
+    public override void OnTouchStart(Entity toucher)
+    {
+        base.OnTouchStart(toucher);
+        
+        toucher.Tags.Add(Garryware.Tags.OnBox);
+    }
+
+    public override void OnTouchEnd(Entity toucher)
+    {
+        base.OnTouchEnd(toucher);
+        
+        toucher.Tags.Remove(Garryware.Tags.OnBox);
+    }
+}
