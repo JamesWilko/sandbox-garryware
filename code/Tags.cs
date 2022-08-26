6fb56835d93144eab16ef85462bf42360619cf38
James Wilkinson <jw@jameswilko.com>
2022-08-26 22:08:10 +0200

    Fix typo in Tags causing points to not work properly

--- code/Tags.cs
+++ code/Tags.cs
@@ -5,7 +5,7 @@ public static class Tags
     public const string OnBox = "on_box";
 
     public const string Points = "points";
-    public const string Place = "points";
+    public const string Place = "place";
     public const string Streak = "streak";
     public const string MaxStreak = "max-streak";
 }
