2ba2576a64e505cb156022f2804a8b18975d797d
Will Donohoe <will@mcsi.mp>
2022-08-28 00:32:32 +1000

    Modify ShuffledDeck to avoid repeats in Next()

--- code/Utility/ShuffledDeck.cs
+++ code/Utility/ShuffledDeck.cs
@@ -6,18 +6,18 @@ namespace Garryware;
 
 public class ShuffledDeck<T>
 {
-    private readonly List<T> allElements = new();
     private readonly List<T> shuffledElements = new();
+    private readonly List<T> usedElements = new();
     private readonly Random random = new();
 
-    public int Count => allElements.Count;
+    public int Count => shuffledElements.Count + usedElements.Count;
     public int Remaining => shuffledElements.Count;
-    
+
     public void Add(T element, int count = 1)
     {
         for (int i = 0; i < count; ++i)
         {
-            allElements.Add(element);
+            usedElements.Add(element);
         }
     }
 
@@ -28,31 +28,47 @@ public class ShuffledDeck<T>
             Add(element, count);
         }
     }
-    
+
+    public void Clear()
+    {
+        shuffledElements.Clear();
+        usedElements.Clear();
+    }
+
     public T Next()
     {
+        // If there's nothing left in the deck, add all the discarded cards and shuffle.
+        // If there's one thing left, shuffle the discards then add them. This is to avoid
+        // repeating the same thing twice.
         if (shuffledElements.Count == 0)
         {
             Shuffle(true);
         }
-        
+        else if (shuffledElements.Count == 1)
+        {
+            ShuffleList(usedElements);
+            shuffledElements.AddRange(usedElements);
+            usedElements.Clear();
+        }
+
         var element = shuffledElements.First();
         shuffledElements.RemoveAt(0);
+        usedElements.Add(element);
         return element;
     }
 
     public void Shuffle(bool returnCards = true)
     {
-        if (returnCards || shuffledElements.Count < 2)
+        if (returnCards)
         {
-            shuffledElements.Clear();
-            shuffledElements.AddRange(allElements);
+            shuffledElements.AddRange(usedElements);
+            usedElements.Clear();
         }
-        Shuffle(shuffledElements);
+        ShuffleList(shuffledElements);
     }
-    
-    private void Shuffle(IList<T> list)  
-    {  
+
+    private void ShuffleList(IList<T> list)
+    {
         int n = list.Count;
         while (n > 1)
         {
