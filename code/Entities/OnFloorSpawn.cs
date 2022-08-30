418415ef462829debd9b3321096fce174f2472f4
James Wilkinson <jw@jameswilko.com>
2022-08-30 21:40:26 +0200

    Add on floor spawn entity

---
+++ code/Entities/OnFloorSpawn.cs
@@ -0,0 +1,21 @@
+﻿using Sandbox;
+using SandboxEditor;
+
+namespace Garryware.Entities;
+
+public enum FloorGridColor
+{
+    Light,
+    Dark
+}
+
+/// <summary>
+/// A spawn point for entities on the floor of the map.
+/// </summary>
+[Library("gw_spawn_on_floor"), HammerEntity]
+[Title("On Floor Spawn")]
+public partial class OnFloorSpawn : Entity
+{
+    [Property, DefaultValue(FloorGridColor.Light)]
+    public FloorGridColor GridColor { get; set; }
+}
