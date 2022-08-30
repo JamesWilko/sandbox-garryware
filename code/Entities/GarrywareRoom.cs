641d89164650bfb21618b6727e89e1f82ffd37c6
James Wilkinson <jw@jameswilko.com>
2022-08-30 20:19:45 +0200

    Add some new map entities, GarrywareRoom and InAirSpawn

---
+++ code/Entities/GarrywareRoom.cs
@@ -0,0 +1,28 @@
+﻿using Sandbox;
+using SandboxEditor;
+
+namespace Garryware.Entities;
+
+public enum RoomSize
+{
+    Small, // 0 - 6 players
+    Medium, // 6 - 12 players
+    Large // 13+ players
+}
+
+public enum RoomType
+{
+    Empty,
+    Boxes,
+    Platform
+}
+
+[Library("gw_trigger_room"), HammerEntity]
+[Title("Garryware Room Extents")]
+public class GarrywareRoom : BaseTrigger
+{
+
+    public RoomSize Size;
+    public RoomType Type;
+
+}
---
+++ code/Entities/InAirSpawn.cs
@@ -0,0 +1,13 @@
+﻿using Sandbox;
+using SandboxEditor;
+
+namespace Garryware.Entities;
+
+/// <summary>
+/// A spawn point for entities that is high in the air of the minigame room.
+/// </summary>
+[Library("gw_spawn_in_air"), HammerEntity]
+[Title("In Air Spawn")]
+public partial class InAirSpawn : Entity
+{
+}
