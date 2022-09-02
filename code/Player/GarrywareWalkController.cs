ab3631e7bf2ed0ca6729895a6c0fe2334f9a7281
James Wilkinson <jw@jameswilko.com>
2022-09-02 15:21:06 +0200

    Add LauncherPistol weapon

--- code/Player/GarrywareWalkController.cs
+++ code/Player/GarrywareWalkController.cs
@@ -2,11 +2,23 @@
 
 namespace Garryware;
 
-public class GarrywareWalkController : WalkController
+public partial class GarrywareWalkController : WalkController
 {
+    [Predicted] public Vector3 KnockbackVelocity { get; protected set; }
+    
     public override void Simulate()
     {
         base.Simulate();
+        
+        // Apply knockback to the players if it has been set
+        if (KnockbackVelocity.LengthSquared > 1.0f)
+        {
+            GroundEntity = null;
+            Velocity += KnockbackVelocity;
+            Move();
+            
+            KnockbackVelocity = Vector3.Zero;
+        }
 
         // Set player's ready up state
         if (Input.Pressed(InputButton.Flashlight))
@@ -14,4 +26,11 @@ public class GarrywareWalkController : WalkController
             GarrywareGame.TogglePlayerReadyState();
         }
     }
+
+    public void Knockback(Vector3 velocity)
+    {
+        KnockbackVelocity = velocity;
+        AddEvent("jump");
+    }
+    
 }
---
+++ code/Weapons/LauncherPistol.cs
@@ -0,0 +1,63 @@
+﻿using Sandbox;
+
+namespace Garryware;
+
+partial class LauncherPistol : AmmoWeapon
+{
+    public override string ViewModelPath => "weapons/rust_pistol/v_rust_pistol.vmdl";
+
+    public override float PrimaryRate => 0.5f;
+
+    public override int MagazineCapacity => 1;
+
+    public override void Spawn()
+    {
+        base.Spawn();
+        SetModel("weapons/rust_pistol/rust_pistol.vmdl");
+    }
+
+    public override bool CanPrimaryAttack()
+    {
+        return base.CanPrimaryAttack() && Input.Pressed(InputButton.PrimaryAttack);
+    }
+
+    public override void AttackPrimary()
+    {
+        (Owner as AnimatedEntity)?.SetAnimParameter("b_attack", true);
+
+        ShootEffects();
+        PlaySound("rust_pistol.shoot");
+        ShootBullet(0f, 1.5f, 0f, 3.0f);
+        TakeAmmo(1);
+    }
+    
+    public override void ShootBullet(Vector3 pos, Vector3 dir, float spread, float force, float damage, float bulletSize)
+    {
+        var forward = dir;
+        forward += (Vector3.Random + Vector3.Random + Vector3.Random + Vector3.Random) * spread * 0.25f;
+        forward = forward.Normal;
+        
+        foreach (var tr in TraceBullet(pos, pos + forward * 5000, bulletSize))
+        {
+            // @todo: effects
+
+            if (!IsServer) continue;
+            if (!tr.Entity.IsValid()) continue;
+            
+            if (tr.Entity is Player player)
+            {
+                if (player.Controller is GarrywareWalkController controller)
+                {
+                    controller.Knockback(Vector3.Up * 700.0f);
+                }
+            }
+            else
+            {
+                using (Prediction.Off())
+                    tr.Entity.ApplyAbsoluteImpulse(Vector3.Up * 500.0f);
+            }
+
+        }
+    }
+    
+}
